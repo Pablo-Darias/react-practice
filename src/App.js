751736b2ee4ba8521c38867318c545d9f47dd635
Pablo-Darias <pablo.darias@s4rb.com>
2020-10-21 13:08:20 +0100

    Completing lesson 1 of LRMWA

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Greeting } from './Greeting'
+import { PeopleList } from './PeopleList'
 import './App.css';
 
+const people = [
+  {
+    name: 'John',
+    age: 40,
+    hairColor: 'brown'
+  },
+  {
+    name: 'Helga',
+    age: 45,
+    hairColor: 'red'
+  },
+  {
+    name: 'Dwayne',
+    age: 55,
+    hairColor: 'blonde'
+  }
+]
+
 function App() {
+  let adjective = 'cool';
+  let url = 'https://reactjs.org'
+  
   return (
     <div className="App">
       <header className="App-header">
+        <Greeting name="Pablo" numberOfMessages={5} />
+        <PeopleList people={people} />
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => alert("HELLO")}>Click Me!</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is {adjective}!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={url}
           target="_blank"
           rel="noopener noreferrer"
         >
